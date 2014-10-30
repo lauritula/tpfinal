74cec64dfa8d97727fbbb08b2628f73982886039
Matias Sanchez <matias.l.sanchez@gmail.com>
2014-10-30 17:04:03 -0300

    Ordenando codigo
    junte en el mismo archivo el style menuBusqued con elprincial de
    style.css

--- final/php/index.php
+++ final/php/index.php
@@ -137,7 +137,6 @@ function diaFormato($dias){
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="../css/bootstrap-theme.min.css">
 	<link rel="stylesheet" type="text/css" href="../css/typeaheadjs.css">
-	<link rel="stylesheet" type="text/css" href="../css/menuBusqueda.css">
 	<link rel="stylesheet" type="text/css" href="../css/style.css">
 	
 	<link rel="stylesheet" type="text/css" href="../css/datepicker3.css">
