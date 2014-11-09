5054f5c0fff220523c0c536143d6fc330435f2ed
JuliRabu <julirabu@gmail.com>
2014-11-09 18:13:42 -0300

    estructura

--- final/php/resultadoReserva.php
+++ final/php/resultadoReserva.php
@@ -19,13 +19,31 @@ include "class.php";
 	<link rel="stylesheet" type="text/css" href="../css/datepicker3.css">
 	<link href='http://fonts.googleapis.com/css?family=Roboto:700,500italic,300' rel='stylesheet' type='text/css'>
 	
-	<title>Resultado de b&uacute;squeda de reserva</title>
+	<title>Resultado de b&uacute;squeda de reserva!</title>
 </head>
 
 <body>
 	<nav class="navbar navbar-inverse" role="navigation">
 		<ul  class="nav navbar-nav">
 			<li class="active"><a href="index.php">Home</a></li>
-		</ul>
-
+		</ul>	
 	</nav>
+
+
+
+
+
+
+	<footer class="bs-docs-footer col-md-12" role="contentinfo">
+
+		<p>Calle xxxx - xxxx - localidad
+			( B1714ALE ) - Buenos Aires - Argentina</p>
+			<p>Tel/Fax: ( 54-11 ) 4444-4444 / 5555-5555</p>
+			<p>Mail:  mail@mail.com.ar</p>
+			<p>Web:   http://www.php.com.ar</p>
+
+		</footer>
+
+		<script src="../js/script.js"></script>
+	</body>
+	</html>
