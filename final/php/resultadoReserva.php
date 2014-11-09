14b9a05ca6b0329dd605dc69fa8bf94945dc4821
JuliRabu <julirabu@gmail.com>
2014-11-09 18:00:09 -0300

    nueva
    pagina de resultado de busqueda d reserva

---
+++ final/php/resultadoReserva.php
@@ -0,0 +1,30 @@
+<?php 
+include "class.php";
+ ?>
+
+ <head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<script type="text/javascript" src="../js/jquery.js"></script>
+	<script type="text/javascript" src="../js/moment.js"></script>
+	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="../js/typeahead.min.js"></script>
+	<script type="text/javascript" src="../js/bootstrap-datepicker.js"></script>
+	<script type="text/javascript" src="../js/bootstrap-datepicker.es.js"></script>
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/typeaheadjs.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	
+	<link rel="stylesheet" type="text/css" href="../css/datepicker3.css">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:700,500italic,300' rel='stylesheet' type='text/css'>
+	
+	<title>Resultado de b&uacute;squeda de reserva</title>
+</head>
+
+<body>
+	<nav class="navbar navbar-inverse" role="navigation">
+		<ul  class="nav navbar-nav">
+			<li class="active"><a href="index.php">Home</a></li>
+		</ul>
+
+	</nav>
