74a5b7b42a6050ae237598dd49a21dc621eaf0d1
Matias Sanchez <matias.l.sanchez@gmail.com>
2014-11-28 21:47:22 -0300

    archivo de pagina empleados ( vacio )

---
+++ final/php/empleados.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<script type="text/javascript" src="../js/jquery.js"></script>
+	<script type="text/javascript" src="../js/moment.js"></script>
+	<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="../js/typeahead.min.js"></script>
+	<script type="text/javascript" src="../js/bootstrap-datepicker.js"></script>
+	<script type="text/javascript" src="../js/bootstrap-datepicker.es.js"></script>
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="../css/typeaheadjs.css">
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	
+	<link rel="stylesheet" type="text/css" href="../css/datepicker3.css">
+	<link href='http://fonts.googleapis.com/css?family=Roboto:700,500italic,300' rel='stylesheet' type='text/css'>
+	
+	<title>Adminitracion</title>
+</head>
+<body>
+<nav class="navbar navbar-inverse" role="navigation">
+		<ul  class="nav navbar-nav">
+			<li class="active"><a href="index.php">Home</a></li>
+		</ul>
+
+	</nav>
+
+<form method="post" action="encuesta.php">
+		<div style="width: 200px;">
+			<div>Ingrese DNI<input TYPE='number' name='user'></div>
+			<div>Ingrese contrase&ntilde;a(solo numeros)<input TYPE='password' name='pass'></div>
+			<div><input TYPE='submit' name="boton" value='login'> <input TYPE='submit' name="boton"value='crear'></div>
+
+		</div>
+	</form>
+
+
+<footer class="bs-docs-footer col-md-12" role="contentinfo">
+		<p>Universidad Nacional de La Matanza</p>
+		<p> Programacion Web 2 - Trabajo Practico Final</p>
+		<p>Metallo, M. / Rabuñal, J. / Sanchez, M. / Tula, L.</p>
+		<p>2C 2014</p>
+	</footer>
+</body>
+</html>
