2ba461b2b1f2878968dda9c3a0a4146c59469a90
Matias Sanchez <matias.l.sanchez@gmail.com>
2014-12-10 11:44:07 -0300

    pagina empleados terminada

--- final/php/empleados.php
+++ final/php/empleados.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 include "class.php";
 ?>
 <html>
@@ -27,48 +28,102 @@ include "class.php";
 		</ul>
 
 	</nav>
-	<div class = "text-center col-md-4 col-md-offset-4 ">
-	 <legend class="text-center">PANEL DE CONTROL</legend>
-		<form action='empleados.php' method='post' role='search' >
-			<input type='submit' id='tirarReservas'NAME='tirarReservas' value='Eliminar reserva' class=' col-md-12 btn btn-info'>
-
-			<input type='submit' id='vacantes'NAME='vacantes' value='Notificar Vacantes' class=' col-md-12 btn btn-info'>
-	
-			<input type='submit' id='graficos'NAME='graficos' value='Graficos semanales' class=' col-md-12 btn btn-info'>
-		</form>
-	</div>
 	<?php 
-	$hoy = date('Y-m-d');
-	if (isset($_POST['graficos'])) {
+	
+	if (!isset($_SESSION['idUsuario'])) 
+
+{
+echo "<div class='well pago-box'>
+		<form id='pagoTarjetaFormulario' action='empleados.php' method='post' role='search'>
+			<legend>Login</legend>
+			<div  id='the-basics' >
+				
+				<div class='form-group' >
+					<label for='username-email'>Titular de la tarjeta</label>
+					<input type='text'  id='user' class='  form-control ' name='user' placeholder='usuario'>
+				</div>				
+				<div class='form-group'>
+					<label for='password'>password</label>
+					<input type='password'  id='pass' class='  form-control ' name='pass' placeholder='(password)'>
+				</div>				
+				<input type='submit' id='login' name='login'value='Ingresar' class=' col-md-12 btn btn-success btn-pago-submit'   />
+			</form>";
+}
+	///////////////////////////////////////////////
+	if (isset($_POST['login'])) 
+	{
+
+		$user=$_POST['user'];
+		$pass=$_POST['pass'];
+		
+		///
+		$objConexion = new conexion();
+		$objConexion->conectar("tpfinal");
+		$login=$objConexion->query("select * from usuario where user='$user' and pass='$pass'");
+		////
+		if (mysql_num_rows($login)>0)
+		{ 
+
+			$_SESSION['idUsuario']=$user;
+			header('location: empleados.php');
+
+		}
+	}
+
+			if (isset($_SESSION['idUsuario'])) 
+		{
+			echo "<div class = ' panel panel-info text-center col-md-4 col-md-offset-4 '>
+			<form action='empleados.php' method='post' role='search' >
+			 <legend class='text-center'>PANEL DE CONTROL <button class='btn btn-danger col-md-2 col-md-offset-4' style='float: right;' type='submit' name='salir' >exit</button></legend>
+				
+					<input type='submit' id='tirarReservas'NAME='tirarReservas' value='Eliminar reserva' class=' col-md-12 btn btn-info'>
+
+					<input type='submit' id='vacantes'NAME='vacantes' value='Notificar Vacantes' class=' col-md-12 btn btn-info'>
+			
+					<input type='submit' id='graficos'NAME='graficos' value='Graficos semanales' class=' col-md-12 btn btn-info'>
+				</form>
+			</div>";
+			
+			$hoy = date('Y-m-d');
+				if (isset($_POST['graficos'])) {
 
 
-		echo "<div>
-		<img src='graficoCaidosVendidos.php'>
-		</div>";
-		echo "<div>
-		<img src='graficoPorCategoria.php'>
-		</div>";
-		echo "<div>
-		<img src='graficoPasajerosPorVuelos.php'>
-		</div>";
-	}
-	if (isset($_POST['tirarReservas'])) {
-		$objReserva = new reserva(000000);
-		$objReserva->tirarReservasMasivas();
-		echo" <div class='col-md-12'>
-		$objReserva->datosCaidos
-		</div>";
-		echo "<div class='col-md-12'>
-		$objReserva->vacantesDisponibles
-		</div>";
-	}
-	if (isset($_POST['vacantes'])) {
-		$objReserva = new reserva(000000);
-		$objReserva->listaEspera();
-		echo "<div class='col-md-12'>
-		$objReserva->vacantesDisponibles
-		</div>";
-	}
+					 echo "<div class='text-center col-md-12'>
+					 	<div>
+						<img src='graficoCaidosVendidos.php'>
+						</div>
+						 <div>
+						<img src='graficoPorCategoria.php'>
+						</div>
+						 <div>
+						<img src='graficoPasajerosPorVuelos.php'>
+						</div>
+					</div>";
+				}
+				if (isset($_POST['tirarReservas'])) {
+					$objReserva = new reserva(000000);
+					$objReserva->tirarReservasMasivas();
+					echo" <div class='col-md-12'>
+					$objReserva->datosCaidos
+					</div>";
+					echo "<div class='col-md-12'>
+					$objReserva->vacantesDisponibles
+					</div>";
+				}
+				if (isset($_POST['vacantes'])) {
+					$objReserva = new reserva(000000);
+					$objReserva->listaEspera();
+					echo "<div class='col-md-12'>
+					$objReserva->vacantesDisponibles
+					</div>";
+				}
+		
+	
+			}
+			if (isset($_POST['salir'])) {
+					session_destroy();
+					header('location: empleados.php');
+				}
 	?>
 
 
